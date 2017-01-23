cedf2d850b90cc269f2bc4c0b8f2df305678000b
Elio Tohme <eliotohme@gmail.com>
2017-01-23 15:22:28 +0200

    modification for testing basic workfow of the app

--- app/src/main/java/xms/com/vodmobile/MainActivity.java
+++ app/src/main/java/xms/com/vodmobile/MainActivity.java
@@ -2,8 +2,6 @@ package xms.com.vodmobile;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import xms.com.vodmobile.VideoListAndDetails.VideoListActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
