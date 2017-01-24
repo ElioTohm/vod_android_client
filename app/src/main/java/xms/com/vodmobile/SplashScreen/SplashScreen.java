22e49e3a10a2d68db49e92db93b41c5ccb8a111a
Elio Tohme <eliotohme@gmail.com>
2017-01-24 14:40:36 +0200

    modification for testing basic workfow of the app

--- app/src/main/java/xms/com/vodmobile/SplashScreen/SplashScreen.java
+++ app/src/main/java/xms/com/vodmobile/SplashScreen/SplashScreen.java
@@ -1,11 +1,7 @@
 package xms.com.vodmobile.SplashScreen;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
