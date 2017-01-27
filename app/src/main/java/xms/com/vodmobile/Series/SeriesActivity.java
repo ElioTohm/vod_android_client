d5de902655e72294a2a9406d03b82e441c4d975e
Elio Tohme <eliotohme@gmail.com>
2017-01-27 16:52:24 +0200

    client authentication and login done

---
+++ app/src/main/java/xms/com/vodmobile/Series/SeriesActivity.java
@@ -0,0 +1,15 @@
+package xms.com.vodmobile.Series;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import xms.com.vodmobile.R;
+
+public class SeriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_series);
+    }
+}
